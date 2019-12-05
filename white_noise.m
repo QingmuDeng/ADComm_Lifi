648017966825707acd4cd0c0624d73b1c161b68f
Daniel Connolly <dconnolly@olin.edu>
2019-12-04 23:05:04 -0500

    adds more PAM analysis

--- white_noise.m
+++ white_noise.m
@@ -1,7 +1,8 @@
+rng(42);
 N=100000;
 % Generate an array of white noise
 dat = (sign(randn(N,1))+j*sign(randn(N,1)))*1/sqrt(2);
 
-write_usrp_data_file(dat);
+write_usrp_data_file(dat, 'white_noise.dat');
 
-read = read_usrp_data_file
+%read = read_usrp_data_file
