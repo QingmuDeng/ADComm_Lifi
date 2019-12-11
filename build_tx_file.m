4fe783b6ed590f825163fe64a25458b7bca7b43e
Daniel Connolly <dconnolly@olin.edu>
2019-12-10 19:51:31 -0500

    Adds function for generating data file with pilot sequence

---
+++ build_tx_file.m
@@ -0,0 +1,13 @@
+function build_tx_file(txname, data, pilot_len, rrc, T)
+    rng(1);
+    pilot_seq=sign(sign(randn(pilot_len,1))+1i*sign(randn(pilot_len,1)));
+
+    rng(42);
+    rrc = rrc ./ max(rrc);
+    dat = [pilot_seq.' data.'].';
+    x_us=upsample(dat,T);
+    pulse = rrc;
+    x=conv(x_us,pulse,'same');
+
+    write_usrp_data_file(x,txname)
+end
