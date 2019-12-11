f7e825b78f3ff941a8b729a5a2d3845550cb9a09
Daniel Connolly <dconnolly@olin.edu>
2019-12-11 18:36:15 -0500

    Adds reverted qpsk file

--- qpsk.m
+++ qpsk.m
@@ -26,9 +26,9 @@ tx_data = tx_data(pilot_sequence_len*T:end-1);
 tx_conv = conv(tx_data,rrc,'same');
 tx_ds = downsample(tx_conv,T);
 
-manual_phase_offsets = linspace(0, 2*pi, 16);
-best_phase = 0;
-error_rate = 1;
+% manual_phase_offsets = linspace(0, 2*pi, 16);
+% best_phase = 0;
+% error_rate = 1;
 
 % pilot_offset = rx_pilot ./ tx_pilot
 % pilot_offset
@@ -51,18 +51,20 @@ error_rate = 1;
 % best_phase
 % final_offset = time(offset,exp(best_phase));
 % read_offset=times(signal_rx,final_offset.');
-% read_offset = times(signal_rx,offset.');
-rx_costas = costas(signal_rx);
+read_offset = times(signal_rx,offset.');
+% rx_costas = costas(signal_rx);
 
 % Downsample RX signal
-rx_ds = downsample(rx_costas,T);
+% rx_ds = downsample(rx_costas,T);
+rx_ds = downsample(read_offset,T);
 
 % Plot real vs imaginary of adjusted rx signal
-figure
-plot(real(rx_costas),imag(rx_costas),'.')
+figure;
+plot(real(read_offset),imag(read_offset),'.')
 title('RX Signal Adjusted for Phase - Re vs Im')
 xlabel('Real Component')
 ylabel('Imaginary Component')
-err_rate = "HAHA";
-
+% err_rate = "HAHA";
+figure;
 plot(real(rx_ds), imag(rx_ds),'.')
+err_rate = compute_qpsk_error(tx_ds, rx_ds);
