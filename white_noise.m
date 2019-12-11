3e7a71e052f67c4e3259919098355d80adb1fb7c
wFairmanOlin <31521968+wFairmanOlin@users.noreply.github.com>
2019-12-10 20:26:30 -0500

    added image QAM file

--- white_noise.m
+++
@@ -1,8 +0,0 @@
-rng(42);
-N=100000;
-% Generate an array of white noise
-dat = (sign(randn(N,1))+j*sign(randn(N,1)))*1/sqrt(2);
-
-write_usrp_data_file(dat, 'white_noise.dat');
-
-%read = read_usrp_data_file
